434fcfb77cf79d53c5646b721913543be9647ca6
Adrien Lapasset <aflapasset@gmail.com>
2020-08-26 14:36:10 +0200

    participer page

---
+++ slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  var slider = tns({
+    container: '.slider',
+    // "center": true,
+    autoWidth: true,
+    controlsContainer: ".slider-controls",
+    mouseDrag: true,
+    loop: true,
+    // swipeAngle: false,
+    nav: false,
+    gutter: 90,
+    // viewportMax: true,
+  });
+
+})
+
