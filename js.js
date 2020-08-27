ac637345377e1c879946c02e4bc87ed49977423e
Adrien Lapasset <aflapasset@gmail.com>
2020-08-27 10:12:32 +0200

    add redirect when submit choco-mood

--- js.js
+++ js.js
@@ -2,5 +2,17 @@ $(document).ready(function () {
 
   $('[data-toggle="tooltip"]').tooltip()
 
+  $("#choco-moods").submit(function (event) {
+    var chocoMoods = document.getElementsByName('choco-mood');
+    for (var i = 0, length = chocoMoods.length; i < length; i++) {
+      if (chocoMoods[i].checked) {
+        window.location.replace(chocoMoods[i].value + '.html');
+        break;
+      }
+    }
+    event.preventDefault();
+  });
+
+
 })
 
