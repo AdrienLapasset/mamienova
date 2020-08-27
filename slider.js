edcf8b729a6ed7816a2302c1a8b2d7e280adc4fd
Adrien Lapasset <aflapasset@gmail.com>
2020-08-27 09:09:37 +0200

    fixes

--- slider.js
+++ slider.js
@@ -2,15 +2,12 @@ $(document).ready(function () {
 
   var slider = tns({
     container: '.slider',
-    // "center": true,
     autoWidth: true,
     controlsContainer: ".slider-controls",
     mouseDrag: true,
     loop: true,
-    // swipeAngle: false,
     nav: false,
     gutter: 90,
-    // viewportMax: true,
   });
 
   $('[data-toggle="tooltip"]').tooltip()
